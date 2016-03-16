7f2615d2b303f5f266da65839d6c053231b1c974
Samuel Molnar <molnar.samuel@gmail.com>
2016-03-16 17:12:30 +0100

    Fix few stuff

--- app/components/prosecutors-list.js
+++ app/components/prosecutors-list.js
@@ -4,6 +4,5 @@ import Foldable from '../mixins/foldable';
 export default Ember.Component.extend(Foldable, {
   sortDefinition: ['name.last', 'name.first'],
   sortedProsecutors: Ember.computed.sort('prosecutors', 'sortDefinition'),
-  foldableContent: Ember.computed.alias('sortedProsecutors'),
-
+  foldableContent: Ember.computed.alias('sortedProsecutors')
 });
