e53256ff26ea727cd3c50e0569e6f908029cc76c
Samuel Molnar <molnar.samuel@gmail.com>
2016-03-16 14:20:50 +0100

    Implement search for prosecutors

--- app/routes/prosecutors.js
+++ app/routes/prosecutors.js
@@ -2,9 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   repository: Ember.inject.service('prosecutors-repository'),
+  queryParams: {
+    q: {
+      refreshModel: true
+    }
+  },
 
-  model() {
-    return this.get('repository').all();
+  model(params) {
+    if (params.q) {
+      return this.get('repository').search(params.q);
+    } else {
+      return this.get('repository').all();
+    }
   },
 
   setupController(controller, model) {
--- app/services/prosecutors-repository.js
+++ app/services/prosecutors-repository.js
@@ -12,5 +12,17 @@ export default Ember.Service.extend({
 
   all() {
     return this.get('data');
+  },
+
+  search(query) {
+    return new Promise((resolve, reject) => {
+      this.get('data').then((data) => {
+        var filtered = data.filter(function(e) {
+          return e.name.value.toLowerCase().indexOf(query.toLowerCase()) !== -1 ? true : false;
+        });
+
+        resolve(filtered);
+      })
+    });
   }
 });
