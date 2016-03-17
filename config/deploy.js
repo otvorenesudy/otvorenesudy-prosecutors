88594a7c7e58c788274cd47400c2a9efb2893f13
Samuel Molnar <molnar.samuel@gmail.com>
2016-03-17 13:41:48 +0100

    Add deployment config

---
+++ config/deploy.js
@@ -0,0 +1,41 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {
+      environment: deployTarget,
+      outputPath: 'dist'
+    },
+
+    'revision-data': {
+      type: 'git-commit'
+    }
+  };
+
+  if (deployTarget === 'production') {
+    var ssh = {
+      host: 'otvorenesudy.sk',
+      username: 'deploy',
+      password: process.env.OTVORENESUDY_DEPLOY_PASSWORD
+    }
+
+    ENV['ssh-index'] = {
+      host: ssh.host,
+      username: ssh.username,
+      remoteDir: '/home/deploy/projects/otvorenesudy-prosecutors-production/',
+      password: ssh.password,
+      allowOverwrite: true
+    };
+
+    ENV.rsync = {
+      ssh: true,
+      host: `${ssh.username}@${ssh.host}`,
+      password: ssh.password,
+      dest: ENV['ssh-index'].remoteDir,
+      delete: false,
+      recursive: true
+    };
+  }
+
+  return ENV;
+};
