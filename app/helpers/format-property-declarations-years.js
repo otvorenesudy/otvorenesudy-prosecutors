a781151711acd966b974bf2e03c81c854b0cff30
Samuel Molnar <molnar.samuel@gmail.com>
2016-03-18 18:15:20 +0100

    Fix unused parameter

--- app/helpers/format-property-declarations-years.js
+++ app/helpers/format-property-declarations-years.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export function formatPropertyDeclarationsYears(params, hash) {
+export function formatPropertyDeclarationsYears(params) {
   var declarations = params[0];
 
   return declarations.mapBy('year').join(', ');
