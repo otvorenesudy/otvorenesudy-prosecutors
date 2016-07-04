6ce0b26ed2b77d11c95cb89ee2199f624821fa0e
Samuel Molnar <molnar.samuel@gmail.com>
2016-07-04 11:23:32 -0300

    Update ember

--- app/router.js
+++ app/router.js
@@ -2,11 +2,14 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('prosecutors', { path: '/' });
+  this.route('prosecutors', { path: '/' }, function() {
+    this.route('refine');
+  });
   this.route('prosecutor', { path: '/:name' });
 });
 
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'opencourts-prosecutors',
     podModulePrefix: 'opencourts-prosecutors/pods',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -34,7 +34,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
