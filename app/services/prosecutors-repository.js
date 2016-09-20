f7d51c48795dc2697df0bedfa4f62605d2301111
Samuel Molnar <molnar.samuel@gmail.com>
2016-09-20 23:53:46 +0200

    Update data

--- app/services/prosecutors-repository.js
+++ app/services/prosecutors-repository.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export default Ember.Service.extend({
   data: null,
-  url: 'https://raw.githubusercontent.com/otvorenesudy/otvorenesudy-data/master/prosecutors-property-declarations-20160815161604.json',
+  url: 'https://raw.githubusercontent.com/otvorenesudy/otvorenesudy-data/master/prosecutors-property-declarations-20160920231924.json',
   updatedAt: Ember.computed('url', function() {
     var datetime = this.get('url').match(/(\d{4}\d{2}\d{2}\d{2}\d{2}\d{2}).json$/)[1];
 
